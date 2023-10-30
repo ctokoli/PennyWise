7787e3130369f4d5850f2f10a13f1111febc96ec
ctokoli <matumasie@gmail.com>
2023-10-30 17:06:55 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   has_many :category, foreign_key: "user_id", class_name: "Category", dependent: :destroy
   has_many :expense, foreign_key: "author_id", class_name: "Expense"
 
-  has_one_attached :image
-
-  validates :name, presence: true, length: { minimum: 4, maximum: 50 }
+  
+  validates :name, presence: true
 end
