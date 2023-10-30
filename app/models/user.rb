3dd8786d6253295f97e5279e0f30ccee909abffc
ctokoli <matumasie@gmail.com>
2023-10-30 09:18:27 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :category, foreign_key: "user_id", class_name: "Category", dependent: :destroy
-  has_many :expense, foreign_key: "author_id", class_name: "Expense",
-  
+  has_many :expense, foreign_key: "author_id", class_name: "Expense"
+
   has_one_attached :image
 
   validates :name, presence: true, length: { minimum: 4, maximum: 50 }
