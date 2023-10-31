a98c70f8a2df40a0ce0d8d93067fafe96dc17d8a
ctokoli <matumasie@gmail.com>
2023-10-31 17:53:00 +0000

    Add expenses_controller.rb

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,29 @@
+class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @expenses = Expense.all
+  end
+
+  def new
+    @expense = current_user.expense.build
+  end
+
+  def show
+    @expense = Expense.find_by(id: params[:id])
+  end
+  
+  def create
+    @expense = current_user.expense.build(expense_params)
+    if @expense.save
+      flash[:success] = "Expense created!"
+      redirect_to expenses_path
+    else
+      render 'new'
+    end
+  end
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount, :category_id)
+  end
+end
