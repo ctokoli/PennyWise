f98fab5f221e039ba6a9dac00c95d53193b83cfc
ctokoli <matumasie@gmail.com>
2023-11-01 17:34:16 +0000

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some example to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(@user).to be_a(User)
+      expect(@user).to be_valid
+    end
+  end
+
+  describe 'public instance methods' do
+    context 'should return' do
+      it 'all the users' do
+        expect(User.all).to eq([@user])
+      end
+    end
+  end
 end
