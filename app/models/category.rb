3947dd587ad63686412933710ec89d2379c892e2
ctokoli <matumasie@gmail.com>
2023-11-01 10:31:53 +0000

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationRecord
-  has_many :expenses, foreign_key: "category_id", class_name: "Expense", dependent: :destroy
-  belongs_to :user, foreign_key: "user_id", class_name: "User"
+  has_many :expenses, foreign_key: 'category_id', class_name: 'Expense', dependent: :destroy
+  belongs_to :user, foreign_key: 'user_id', class_name: 'User'
 
   validates :name, presence: true, length: { minimum: 4, maximum: 50 }
-
 end
