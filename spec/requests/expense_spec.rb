cffbe37c1a8ddfd0bd03b12c9f2815a67b2c549e
ctokoli <matumasie@gmail.com>
2023-11-01 17:34:16 +0000

    Update expense_spec.rb

--- spec/requests/expense_spec.rb
+++ spec/requests/expense_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Expenses', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  before(:each) do
+    login_as(user, :scope => :user)
+  end
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      category = FactoryBot.create(:category, user_id: user.id)
+      get "/categories/#{category.id}/expenses/new"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'render the index template' do
+      category = FactoryBot.create(:category, user_id: user.id)
+      get "/categories/#{category.id}/expenses/new"
+      expect(response).to render_template('new')
+    end
   end
 end
