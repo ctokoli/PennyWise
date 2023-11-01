fa8ceb0fd7aa4ae01d9ae9f541864f4aa21bf365
ctokoli <matumasie@gmail.com>
2023-11-01 10:31:53 +0000

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,8 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @categories = Category.all
+    @categories = Category.includes(:expenses).where(user_id: current_user.id)
+    @total_price = total_expenses(:id)
   end
 
   def new
@@ -12,15 +13,27 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by(id: params[:id])
     @expenses = @category.expenses
+    @total_price = @category.expenses.sum(:amount)
   end
 
   def create
     @category = current_user.category.build(category_params)
     if @category.save
-      flash[:success] = "Category created!"
+      flash[:success] = 'Category created!'
       redirect_to categories_path
     else
       render 'new'
+    end
+  end
+
+  private
+
+  def total_expenses(id)
+    @category = Category.find_by(id:)
+    if @category
+      @category.expenses.sum(:amount)
+    else
+      0
     end
   end
 
