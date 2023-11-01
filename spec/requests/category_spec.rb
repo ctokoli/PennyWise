d7de1243fd9525f43f00a4504e40ec8d19c2aac6
ctokoli <matumasie@gmail.com>
2023-11-01 17:34:16 +0000

    Update category_spec.rb

--- spec/requests/category_spec.rb
+++ spec/requests/category_spec.rb
@@ -1,7 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe 'Categories', type: :request do
+
+  let(:user) { FactoryBot.create(:user) }
+
+  before(:each) do
+    login_as(user, :scope => :user)
+  end
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get '/categories'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'render the index template' do
+      get '/categories'
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      category = FactoryBot.create(:category, user_id: user.id)
+      get "/categories/#{category.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'render the show template' do
+      category = FactoryBot.create(:category, user_id: user.id)
+      get "/categories/#{category.id}"
+      expect(response).to render_template('show')
+    end
+  end
+
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/categories/new'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'render the new template' do
+      get '/categories/new'
+      expect(response).to render_template('new')
+    end
   end
 end
